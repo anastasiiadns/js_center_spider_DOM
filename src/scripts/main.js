415276855ee986526bda59adc7df363bf24308e8
anastasiiadns <nastyadenisenko17@gmail.com>
2025-09-08 17:14:24 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+
+const spider = document.querySelector('.spider');
+const spiderWidth = spider.clientWidth;
+const spiderHeight = spider.clientHeight;
+
+const leftIndentCalculation = (wallWidth - spiderWidth) / 2;
+const topIndentCalculation = (wallHeight - spiderHeight) / 2;
+
+if (getComputedStyle(wall).position !== 'relative') {
+  wall.style.position = 'relative';
+}
+
+if (getComputedStyle(spider).position !== 'absolute') {
+  spider.style.position = 'absolute';
+}
+
+spider.style.left = leftIndentCalculation + 'px';
+spider.style.top = topIndentCalculation + 'px';
